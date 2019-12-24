d87ed2edbffe0cd413a044b82e864f1aef702098
Erik Van Slyke <evanslyke31@gmail.com>
2019-12-24 00:12:25 -0600

    third

--- stock.py
+++ stock.py
@@ -7,16 +7,43 @@ def accuracy_score(observed,true):
     diff = 0.0
     for a,b in zip(observed,true):
         diff += abs((float(a) - b)/b)
-    return 1.0 - diff
+    return 1.0 - (diff/len(observed))
 
-#dataset = read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=AAPL&interval=5min&apikey=F4J8FATPAPG9HGF3&datatype=csv")
+#dataset = read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAILY&symbol=AAPL&interval=1min&apikey=F4J8FATPAPG9HGF3&datatype=csv")
 symbols = read_csv("constituents.csv")
 
+forcast_weight = 20
+count = 1
+stock = raw_input("input stock symbol: ")
+while True:
+    if(stock is ''):
+        break
+    data = read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=5min&apikey=F4J8FATPAPG9HGF3&datatype=csv" % (stock))
+    #print sym + str(np.array(data['close']))
+    print type(data)
+    array = data.values
+    Y = array[:,4]
+    Y_train = Y[:len(Y)-forcast_weight]
+    Y_validation = Y[len(Y)-forcast_weight:]
 
-for sym in np.array(symbols['Symbol']):
-    data = read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=%s&interval=5min&apikey=F4J8FATPAPG9HGF3&datatype=csv" % (sym))
-    print np.array(data['close'])
-"""forcast_weight = 100
+    model = ARIMA(Y_train, order=(0, 1, 1))
+    fit = model.fit(disp=False)
+    predictions = fit.predict(len(Y)-forcast_weight,len(Y),typ='levels')
+    predictions = np.array(predictions) 
+
+    print accuracy_score(predictions, Y_validation)
+
+    pyplot.figure(count)
+    pyplot.title(stock)
+    ax = pyplot.axes()
+    ax.plot(range(len(Y)),Y,color='red')
+    ax.plot(range(len(Y) - len(predictions),len(Y)),predictions,color='blue')
+    count += 1
+
+    stock = raw_input("input stock symbol: ")
+pyplot.show()
+
+"""
 
 array = dataset.values
 Y = array[:,4]
